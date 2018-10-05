9d73771327f1c620a1b69e2f399ac6a36af4513c
Jessy Fouace <jessy.fouace62@gmail.com>
2018-10-05 12:05:01 +0200

    Exercice 1 version advanced, wiht protect

--- js/main.js
+++ js/main.js
@@ -15,29 +15,39 @@
 // homme();
 
 
-function homme (prenom, nom, pays, age, payement) {
+function homme(prenom, nom, habitations, ages, payements) {
   var name = prompt("Quel est votre prénom ?");
   this.prenom = name;
   var surname = prompt("Quel est votre nom ?");
   this.nom = surname;
   var lives = prompt("Dans quel pays résidez vous ?");
-  this.lives = ["France", "Angleterre"];
-  var age = prompt("Quel âge avez vous ?");
-  this.age = age;
+  this.habitations = ["France", "Angleterre"];
+  var agess = prompt("Quel âge avez vous ?");
+  this.ages = agess;
   var payement = prompt("Choisisez un moyen de payement (visa, mastercard)");
-  this.payement = ["visa", "mastercard"];
+  this.payements = ["visa", "mastercard"];
   this.getName = function() {
-    var calculage = 2018 - this.age;
-    for (var i = 0; i < this.payement.length; i++) {
-      if (payement == this.payement[i] && lives == this.lives[i] && calculage > 18) {
-        alert("Bonjour " + this.prenom + " " + this.nom + ". Vous êtes née en: " + calculage + ". Et vous habitez en: " + lives);
+    var calculage = 2018 - this.ages;
+    for (i = 0; i < this.habitations.length; i++) {
+      if (lives == this.habitations[i]) {
+        break;
       } else {
-        alert("Le moyen de payement ou le lieu d'habitation ne sont pas bon.")
+        continue;
       }
     }
-      if (calculage < 18) {
-        alert("Vous n'avez pas l'âge requis pour avoir accès à ce site.");
+    for (e = 0; e < this.payements.length; e++) {
+      if (payement == this.payements[e]) {
+        break;
+      } else {
+        continue;
       }
+    }
+
+    if (this.habitations[i] == undefined || this.payements[e] == undefined || this.prenom == "" || this.nom == "" || calculage < 1998) {
+      alert("Il s'emblerait qu'une ou plusieurs de vos informations ne soit pas correctes.")
+    }else{
+      alert("Vous vous appelez: " + this.prenom + " " + this.nom + ". Vous êtes née en: " + calculage + ". Vous habitez en: " + this.habitations[i] + ". Et votre moyen de payement est: " + this.payements[e]);
+    }
   };
   getName();
 }
